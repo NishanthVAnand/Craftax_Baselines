f05e4aa36c40b20b1b15d057b70c4098cfe75055
NishanthVAnand <nishanth127127@gmail.com>
2025-04-11 20:00:33 -0400

    sync

--- text_wrapper_crafter_classic.py
+++ text_wrapper_crafter_classic.py
@@ -50,6 +50,37 @@ Intrinsic_Items = [
     "Energy",
 ]
 Direction = ["Left", "Right", "Up", "Down"]
+Direction_to_obj = {
+    0: (OBS_DIM[0] // 2, OBS_DIM[1] // 2 - 1),
+    1: (OBS_DIM[0] // 2, OBS_DIM[1] // 2 + 1),
+    2: (OBS_DIM[0] // 2 - 1, OBS_DIM[1] // 2),
+    3: (OBS_DIM[0] // 2 + 1, OBS_DIM[1] // 2),
+}
+
+ACHIEVEMENTS = [
+    "Collect Wood",
+    "Place Table",
+    "Eat Cow",
+    "Collect Sampling",
+    "Collect Drink",
+    "Make Wood Pickaxe",
+    "Make Wood Sword",
+    "Place Plant",
+    "Defeat Zombie",
+    "Collect Stone",
+    "Place Stone",
+    "Eat Plant",
+    "Defeat Skeleton",
+    "Make Stone Pickaxe",
+    "Make Stone Sword",
+    "Wake Up",
+    "Place Furnace",
+    "Collect Coal",
+    "Collect Iron",
+    "Make Iron Pickaxe",
+    "Make Iron Sword",
+    "Collect Diamond",
+]
 
 
 def generate_distance_dict(max_range=5):
@@ -90,9 +121,12 @@ def symbolic_to_text_numpy(symbolic_array):
     meta_prompt += "mobs (zombie, cow, arrow, etc), inventory (wood, iron, diamond, etc), intrinsic values (health, food, drink, and energy). "
     meta_prompt += "Maintaining high levels are important for the agent to stay alive. "
     meta_prompt += "You will also receive the brightness level of the environment indicating the time of the day. "
-    meta_prompt += "Your task is to interpret and remember the details of this observation. "
+    meta_prompt += "Your task is to interpret and remember the details of this observation.\n"
+    meta_prompt += (
+        "The agent will then use this information to complete the following achievements: "
+    )
+    meta_prompt += ", ".join(ACHIEVEMENTS) + ". "
     text_description.append(meta_prompt)
-    text_description.append("Observation: ")
 
     rows = np.arange(OBS_DIM[0])[:, None]
     cols = np.arange(OBS_DIM[1])[None, :]
@@ -105,7 +139,10 @@ def symbolic_to_text_numpy(symbolic_array):
     # Block types description
     symbolic_array_map_blocks = symbolic_array_map_reshaped[:, :, :17]
     if symbolic_array_map_blocks.sum() > 0:
-        text_description.append("Block types: ")
+        block_description = "There are a total of 17 different types of blocks: "
+        block_description += ", ".join([Block_id_to_text[i] for i in range(17)])
+        block_description += ". The following blocks appear in your sight: "
+        text_description.append(block_description)
         block_types = np.argmax(symbolic_array_map_blocks, axis=-1)
         unique_blocks = np.unique(block_types)
         unique_blocks = unique_blocks[~np.isin(unique_blocks, [0, 1])]
@@ -119,13 +156,30 @@ def symbolic_to_text_numpy(symbolic_array):
             relative_pos = min_dist_indices - np.array([OBS_DIM[0] // 2, OBS_DIM[1] // 2])
             distance_tuples = [tuple(map(int, d)) for d in relative_pos]
             descriptions = [distance_lookup.get(d, "Unknown movement") for d in distance_tuples]
-            text_description.append(Block_id_to_text[block] + " is at: " + ", ".join(descriptions))
+            text_description.append(Block_id_to_text[block] + " is at " + ", ".join(descriptions))
+
+    # Direction
+    direction_array = symbolic_array[1339:1343]
+    # text_description.append(
+    #     "The agent is facing " + Direction[np.argwhere(direction_array == 1).item()]
+    # )
+    text_description.append(
+        "The agent is facing "
+        + Block_id_to_text[
+            np.argmax(symbolic_array_map_blocks, axis=-1)[
+                Direction_to_obj[np.argwhere(direction_array == 1).item()]
+            ]
+        ]
+    )
 
     # Mob types description
     symbolic_array_map_mobs = symbolic_array_map_reshaped[:, :, 17:21]
     if symbolic_array_map_mobs.sum() > 0:
         mob_types = np.argmax(symbolic_array_map_mobs, axis=-1)
-        text_description.append("Mob types: ")
+        mob_description = "There are a total of 4 different types of mobile objects: "
+        mob_description += ", ".join([mob_id_to_text[i] for i in range(4)])
+        mob_description += ". The following mobile objects appear in your sight: "
+        text_description.append(mob_description)
         unique_mobs = np.unique(mob_types)
         for mob in unique_mobs:
             curr_mob_mask = mob_types == mob
@@ -141,7 +195,12 @@ def symbolic_to_text_numpy(symbolic_array):
 
     inventory_array = np.argwhere(symbolic_array[1323:1335] > 0).flatten()
     if inventory_array.size > 0:
-        text_description.append("Inventory: ")
+        inventory_description = (
+            "The agent can store a total of 12 different types of items in its inventory: "
+        )
+        inventory_description += ", ".join([Inventory_Items[i] for i in range(12)])
+        inventory_description += ". You currently have the following items in your inventory: "
+        text_description.append(inventory_description)
         for inv_idx in inventory_array:
             item_count = symbolic_array[1323:1335][inv_idx]
             text_description.append(
@@ -149,25 +208,21 @@ def symbolic_to_text_numpy(symbolic_array):
             )
 
     intrinsic_array = symbolic_array[1335:1339]
-    text_description.append("Intrinsic values: ")
+    text_description.append(
+        "Below are the intrinsic values of the agent that describes its condition: "
+    )
     for intrinsic_idx in range(len(Intrinsic_Items)):
         intrinsic_value = intrinsic_array[intrinsic_idx]
         desc = ""
         if intrinsic_value < 0.5:
-            desc = "The agent's " + Intrinsic_Items[intrinsic_idx] + " is critical!"
+            desc = " The agent's " + Intrinsic_Items[intrinsic_idx] + " is critical!"
         text_description.append(
-            "The agent's "
-            + Intrinsic_Items[intrinsic_idx]
+            Intrinsic_Items[intrinsic_idx]
             + " is "
             + str(np.ceil(intrinsic_value * 10))
             + "/10."
             + desc
         )
-
-    direction_array = symbolic_array[1339:1343]
-    text_description.append(
-        "The agent is facing " + Direction[np.argwhere(direction_array == 1).item()]
-    )
 
     text_description.append(
         "The light level is " + str(np.around(symbolic_array[1343] * 100, 2)) + "%."
